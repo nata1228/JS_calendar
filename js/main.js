f3419718c7bf06b87730c895b0f0dcfc54cea2d6
Takumi Nakayama <takumi.n19971228@gmail.com>
2023-10-21 07:22:17 +0900

    add js file

---
+++ js/main.js
@@ -0,0 +1,87 @@
+'use strict';
+
+console.clear();
+
+{
+    const year = 2020;
+    const month = 4; //5月
+
+    function getCalendarHead() {
+        const dates = [];
+        const d = new Date(2020, month, 0).getDate();
+        const n = new Date(2020, month, 1).getDay();
+
+        for (let i = 0; i < n; i++) {
+            dates.unshift({
+                date: d - i,
+                isToday: false,
+                isDisabled: true,
+            })
+        }
+        return dates;
+    }
+
+    function getCalendarBody() {
+        const dates = [];
+        const lastDate = new Date(year, month + 1, 0).getDate();
+
+        for (let i = 1; i <= lastDate; i++) {
+            dates.push({
+                date: i,
+                isToday: false,
+                isDisabled: false,
+            });
+        }
+
+        return dates;
+    }
+
+    function getCalendarTail() {
+        const dates = [];
+        const lastDay = new Date(2020, month + 1, 1).getDay();
+        console.log(lastDay);
+
+        for (let i = 1; i < 7 - lastDay; i++) {
+            dates.push({
+                date: i,
+                isToday: false,
+                isDisabled: true,
+            });
+        }
+        return dates;
+    }
+
+    function createCalendar() {
+        const dates = [
+            ...getCalendarHead(),
+            ...getCalendarBody(),
+            ...getCalendarTail()
+        ];
+        const weeks = [];
+        const weeksCount = dates.length / 7;
+
+        for (let i = 0; i < weeksCount; i++) {
+            weeks.push(dates.splice(0, 7));
+        }
+        weeks.forEach(week => {
+            const tr = document.createElement('tr');
+            week.forEach(date => {
+                const td = document.createElement('td');
+
+                td.textContent = date.date;
+                if (date.isToday) {
+                    td.classList.add('today');
+                }
+                if (date.isDisabled) {
+                    td.classList.add('disabled');
+                }
+
+                tr.appendChild(td);
+            })
+            document.querySelector('tbody').appendChild(tr);
+        })
+    }
+
+    createCalendar();
+
+}
